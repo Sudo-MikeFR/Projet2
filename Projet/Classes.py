a0dff9ec7cbfb48cf79ba3f9003210b499fe665d
michael.lagace <michael.lagace2@gmail.com>
2024-06-14 08:07:29 -0400

    Nouvelle version

--- Projet/Classes.py
+++ Projet/Classes.py
@@ -1,9 +1,8 @@
 from typing import List
 
-# Classe personne
+# Classe de base représentant une personne
 class Personne:
     def __init__(self, nom: str, prenom: str, sexe: str):
-        # Initialisation des attributs de base d'une personne
         self.nom = nom
         self.prenom = prenom
         self.sexe = sexe
@@ -11,56 +10,48 @@ class Personne:
 # Classe Client héritant de Personne
 class Client(Personne):
     def __init__(self, nom: str, prenom: str, sexe: str, date_inscription: str, courriel: str, password: str):
-        # Appel du constructeur de la classe parente pour initialiser les attributs de Personne
         super().__init__(nom, prenom, sexe)
-        # Initialisation des attributs spécifiques à Client
-        self.date_inscription = date_inscription
-        self.courriel = courriel
-        self.password = password
-        # Initialisation d'une liste vide pour les cartes de crédit associées
-        self.cartes_credit: List[CarteCredit] = []
+        self.date_inscription = date_inscription  # Date d'inscription
+        self.courriel = courriel  # Courriel unique
+        self.password = password  # Mot de passe
+        self.cartes_credit: List[CarteCredit] = []  # Liste pour les cartes de crédit
 
 # Classe Acteur héritant de Personne
 class Acteur(Personne):
     def __init__(self, nom: str, prenom: str, sexe: str, date_debut_emploi: str, date_fin_emploi: str, nom_personnage: str, salaire: str):
-        # Appel du constructeur de la classe parente pour initialiser les attributs de Personne
         super().__init__(nom, prenom, sexe)
-        # Initialisation des attributs spécifiques à Acteur
-        self.date_debut_emploi = date_debut_emploi
-        self.date_fin_emploi = date_fin_emploi
-        self.nom_personnage = nom_personnage
-        self.salaire = salaire
-        # Initialisation d'une liste vide pour les films associés
-        self.films: List[Film] = []
+        self.date_debut_emploi = date_debut_emploi  # Date de début d'emploi
+        self.date_fin_emploi = date_fin_emploi  # Date de fin d'emploi
+        self.nom_personnage = nom_personnage  # Nom du personnage joué
+        self.salaire = salaire  # Salaire de l'acteur
+        self.films: List[Film] = []  # Liste des films
 
 # Classe Employe héritant de Personne
 class Employe(Personne):
     def __init__(self, nom: str, prenom: str, sexe: str, date_debut_emploi: str, username: str, password: str, type_acces: bool):
-        # Appel du constructeur de la classe parente pour initialiser les attributs de Personne
         super().__init__(nom, prenom, sexe)
-        self.date_debut_emploi = date_debut_emploi
-        self.username = username
-        self.password = password
-        self.type_acces = type_acces
+        self.date_debut_emploi = date_debut_emploi  # Date de début d'emploi
+        self.username = username  # Nom d'utilisateur
+        self.password = password  # Mot de passe
+        self.type_acces = type_acces  # Type d'accès (admin ou non)
 
 # Classe représentant une carte de crédit
 class CarteCredit:
     def __init__(self, numero: str, date_expire: str, secret: str):
-        self.numero = numero
-        self.date_expire = date_expire
-        self.secret = secret
+        self.numero = numero  # Numéro de la carte
+        self.date_expire = date_expire  # Date d'expiration
+        self.secret = secret  # Code secret (3 chiffres)
 
 # Classe représentant un film
 class Film:
     def __init__(self, nom_film: str, duree_film: str, description_film: str):
-        self.nom_film = nom_film
-        self.duree_film = duree_film
-        self.description_film = description_film
-        # Initialisation d'une liste vide pour les catégories associées
-        self.categories: List[Categorie] = []
+        self.nom_film = nom_film  # Nom du film
+        self.duree_film = duree_film  # Durée du film
+        self.description_film = description_film  # Description du film
+        self.categories: List[Categorie] = []  # Liste des catégories
 
-# Classe catégorie de film
+# Classe représentant une catégorie de film
 class Categorie:
     def __init__(self, nom_categorie: str, description_categorie: str):
-        self.nom_categorie = nom_categorie
-        self.description_categorie = description_categorie
+        self.nom_categorie = nom_categorie  # Nom de la catégorie
+        self.description_categorie = description_categorie  # Description de la catégorie
